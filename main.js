6b2a5eb81b29fcc2fc4c5ac5a306a5e1a9299e95
coozin <shouppejosh@gmail.com>
2018-11-02 12:47:49 -0400

    toggle button logic

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+init()
+
+function init () {
+	var close = document.querySelector('#toggle-close');
+	close.style.display = "none";
+}
+
 function toggle () {
-	console.log('toggle')
+	var close = document.querySelector('#toggle-close');
+	var menu = document.querySelector('#toggle-menu');
+
+	if (menu.style.display == "") {
+		menu.style.display = "none";
+		close.style.display = "";
+	} else {
+		menu.style.display = "";
+		close.style.display = "none";
+	}
 }
