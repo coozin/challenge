3a59046c40ff7c35ab594ea572b765b4020be39e
coozin <shouppejosh@gmail.com>
2018-11-02 13:11:33 -0400

    menu logic, menu css

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 init()
 
 function init () {
-	var close = document.querySelector('#toggle-close');
-	close.style.display = "none";
+	var close = document.querySelector('#toggle-close')
+	var dropdown = document.querySelector('#dropdown')
+
+	close.style.display = "none"
+	dropdown.style.display = "none"
 }
 
 function toggle () {
-	var close = document.querySelector('#toggle-close');
-	var menu = document.querySelector('#toggle-menu');
+	var close = document.querySelector('#toggle-close')
+	var menu = document.querySelector('#toggle-menu')
+	var dropdown = document.querySelector('#dropdown')
 
 	if (menu.style.display == "") {
-		menu.style.display = "none";
-		close.style.display = "";
+		menu.style.display = "none"
+		close.style.display = ""
+		dropdown.style.display = ""
 	} else {
-		menu.style.display = "";
-		close.style.display = "none";
+		menu.style.display = ""
+		close.style.display = "none"
+		dropdown.style.display = "none"
 	}
 }
